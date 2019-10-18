61c081ebd58b0db9387a8e746ff6c066f07f3c3f
nathan130200 <nathan130200@gmail.com>
2019-10-18 12:21:51 -0300

    begin implementation localization system.

---
+++ DiscordRichPresence/Entities/DiscordLocalizedConfiguration.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DiscordRichPresence.Entities
+{
+    public class DiscordLocalizedConfiguration
+    {
+        [JsonProperty("strings")]
+        protected List<DiscordLocalizedString> Strings { get; set; } = new List<DiscordLocalizedString>();
+
+        public bool FindString(string key, out DiscordLocalizedString value)
+        {
+            value = null;
+
+            if (string.IsNullOrEmpty(key))
+                return false;
+
+            lock (this.Strings)
+            {
+                var index = this.Strings.FindIndex(x => x.Key.ToLowerInvariant() == key.ToLowerInvariant());
+
+                if (index == -1)
+                    return false;
+
+                value = this.Strings[index];
+                return true;
+            }
+        }
+
+        public bool Add(string key, string value)
+        {
+            if (string.IsNullOrEmpty(key) || string.IsNullOrEmpty(value))
+                return false;
+
+            lock (this.Strings)
+            {
+                var index = this.Strings.FindIndex(x => x.Key.ToLowerInvariant() == key.ToLowerInvariant());
+
+                if (index == -1)
+                    this.Strings.Add(new DiscordLocalizedString { Key = key, Text = value });
+                else
+                {
+                    this.Strings.RemoveAt(index);
+                    this.Strings.Insert(index, new DiscordLocalizedString { Key = key, Text = value });
+                }
+
+                return true;
+            }
+        }
+
+        public bool Remove(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+                return false;
+
+            lock (this.Strings)
+            {
+                var index = this.Strings.FindIndex(x => x.Key.ToLowerInvariant() == key.ToLowerInvariant());
+
+                if (index == -1)
+                    return false;
+
+                this.Strings.RemoveAt(index);
+                return true;
+            }
+        }
+    }
+}
---
+++ DiscordRichPresence/Entities/DiscordLocalizedString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DiscordRichPresence.Entities
+{
+    public class DiscordLocalizedString
+    {
+        public static DiscordLocalizedString FromEmpty(string key)
+        {
+            return new DiscordLocalizedString
+            {
+                Key = key,
+                Text = key
+            };
+        }
+
+        [JsonProperty("text")]
+        public string Key { get; set; }
+
+        [JsonProperty("key")]
+        public string Text { get; set; }
+
+        public string Format(params object[] args)
+        {
+            try { return string.Format(this.Text, args); }
+            catch { return this.Text; }
+        }
+    }
+}
