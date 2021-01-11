0447efb11c826e9686bf66c60106395f902a95f1
roridev <t3ctotalmenterandom1@outlook.com>
2021-01-11 15:27:04 -0300

    Rebranding

--- DiscordRichPresence/Properties/AssemblyInfo.cs
+++ DiscordRichPresence/Properties/AssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Discord")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("gitRPC")]
+[assembly: AssemblyDescription("gitRPC")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Discord")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("gitRPC")]
+[assembly: AssemblyProduct("gitRPC")]
+[assembly: AssemblyCopyright("gitRPC")]
+[assembly: AssemblyTrademark("gitRPC")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
